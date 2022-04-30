bed1375b89e7349544ea15a053f245d4cba72966
antuanumpacto <abarahona@umpacto.com>
2022-04-29 23:07:28 -0500

    Entidad persona

---
+++ src/main/java/com/antuan/first/entities/Hijos.java
@@ -0,0 +1,4 @@
+package com.antuan.first.entities;
+
+public class Hijos {
+}
---
+++ src/main/java/com/antuan/first/entities/Persona.java
@@ -0,0 +1,40 @@
+package com.antuan.first.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "personas")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Persona {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "per_id",nullable = false)
+    private Long perId;
+
+    @Column(name = "per_cedula",nullable = false,length = 10, unique = true)
+    private String perCedula;
+
+    @Column(name = "per_nombre",nullable = false,length = 100)
+    private String perNombre;
+
+    @Column(name = "per_apellido",nullable = false)
+    private String perApellido;
+
+    @Column(name = "per_edad")
+    private Integer perEdad;
+
+    @Column(name = "per_fecha_nacimiento")
+    private Date perFechaNacimiento;
+
+    @Column(name = "per_casado", columnDefinition = "BIT")
+    private Boolean perCasado;
+
+}
